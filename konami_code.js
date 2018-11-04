57ec6cd0daeb763bae4eb160020fc66d4a849f1e
DDarrow123 <ddarrowphl@gmail.com>
2018-11-04 11:33:33 -0500

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,23 @@ const codes = [
   "a"
 ];
 
+
 function init() {
-  // your code here
+let index = 0;
+  function onKeyDown (e){
+
+    if (e.key === codes[index]) {
+      index++;
+
+      if (index === codes.length) {
+
+           alert('Hurray! You entered the right code');
+           index = 0
+      }
+    } else {
+        index = 0
+      }
+}
+const docBody = document.querySelector('body')
+  docBody.addEventListener('keydown', onKeyDown )
 }
